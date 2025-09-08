686486f6a2318e88511bad61fa31c749103aeb9f
barrak2018 <joseestebanojedaaular@gmail.com>
2025-09-07 22:38:23 -0400

    lote de actualizacion deploy en windows

--- deploy.bat
+++ deploy.bat
@@ -3,4 +3,6 @@ echo Deploying application...
 python -m venv venv
 call venv\Scripts\activate
 pip install -r requirements.txt
+mkdir .vscode
+echo * > .vscode\.gitignore
 echo Application deployed successfully.
